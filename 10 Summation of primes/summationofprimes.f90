c51a04f53b94b84dd0b0c54fcc47c678380a2911
nateacoffey <nateacoffey@gmail.com>
2024-08-28 23:18:02 -0700

    Solved problem 10: Using Sieve of Eratosthenes, O(n*log(logn))

--- 10 Summation of primes/summationofprimes.f90
+++ 10 Summation of primes/summationofprimes.f90
@@ -1,64 +1,39 @@
-
+!Find the sum of all the primes below two million
 module math
 	implicit none
 contains
-	function isPrime(n) result(prime)
-		implicit none
-		integer(kind = 8), intent(in) :: n
-		integer(kind=8) :: sqrtNumb, numb
-		integer :: i
-		logical :: prime
-		
-		numb = n
-		!adjust factor formula but if we find a factor, we return false
-		if(n .le. 1)then
-			prime = .false.
-			return
-		else if(n .le. 3) then
-			prime = .true.
-			return
-		end if
-		
-		prime = .true.
-		
-		!cast numb to real64 for the sqrt then cast result to int
-		sqrtNumb = nint(sqrt(real(n, kind=8)))
-
-		!divisible by 2
-		if(MOD(numb, 2) .eq. 0)then
-			prime = .false.
-			return
-		end if
-
-		!divisible by an odd number
-		do i = 3, sqrtNumb, 2
-			if(MOD(numb, i) .eq. 0) then
-				prime = .false.
-				return
-			end if
-		end do
 	
-	end function isPrime
-	
-	function sumOfPrimes(n) result(numb)
+	function sieve_of_eratosthenes(n) result(numb)
 		implicit none
-		integer, intent(in) :: n
-		integer(kind=8) :: i
-		integer(kind=8) :: primeSum
+		integer(kind=8) :: n
+		logical, dimension(2:n) :: isPrime
+		integer :: i, j
 		integer(kind=8) :: numb
 		
-		primeSum = 0
+		isPrime = .true.
 		
-		
-		do i = 2, n-1
+		!start from 2, 
+		!mark every multiple of i after i^2 as not prime
+		do i = 2, int(sqrt(real(n, kind=8)))
 			if(isPrime(i))then
-				primeSum = primeSum + i
+				j = i*i
+				do while(j <= n)
+					isPrime(j) = .false.
+					j = j + i
+				end do
 			end if
-		end do	
+		end do
 		
-		numb = primeSum
+		numb = 0
+		do i = 2, n
+			if(isPrime(i))then
+				numb = numb + i
+			end if
+		end do
 		
-	end function sumOfPrimes
+		
+		
+	end function sieve_of_eratosthenes
 
 end module math
 
@@ -66,12 +41,12 @@ end module math
 program summationofprime
 	use math
 	implicit none
-	integer :: highest
+	integer(kind=8) :: highest
 	integer(kind=8) :: sum
 
 	highest = 2e6
 
-	sum = sumOfPrimes(highest)
+	sum = sieve_of_eratosthenes(highest)
 
 	print *, sum
 
